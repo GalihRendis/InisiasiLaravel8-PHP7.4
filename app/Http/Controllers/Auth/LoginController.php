15d816587acb73a767e5b28386361787d0d89022
GalihRendis <61167412+GalihRendis@users.noreply.github.com>
2022-01-18 15:08:33 +0700

    update login, register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,56 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function viewLogin(){
+        return view('si.login');
+    }
+
+    public function attemptLogin(Request $request){
+
+        $credentials = $request->only('email', 'password', 'remember');
+
+        if(Auth::attempt([
+            'email' => $credentials['email'] ?? null,
+            'password' => $credentials['password'] ?? null],
+            !empty($credentials['remember']))){
+
+            return redirect()->intended('/home');
+        }
+        return redirect()->back()->withInput()
+        ->with('error','Email-Address Or Password Are Wrong.');
+    }
+
+    public function attemptRegister(Request $request){
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8','confirmed', 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/'],
+        ]);
+          // dd($request->all());
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if(Auth::attempt([
+            'email' => $request->email ?? null,
+            'password' => $request->password ?? null])){
+
+            return redirect()->route('home');
+        }
+        return redirect()->route('login')
+        ->with('error','Email-Address Or Password Are Wrong.');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect()->route('register');
+    }
+
+    public function viewRegister() {
+       return view('si.register');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// AUTH Custom
+// Route::get('/register', [App\Http\Controllers\Auth\LoginController::class, 'viewRegister'])->name('register.index');
+// Route::post('/register', [App\Http\Controllers\Auth\LoginController::class, 'attemptRegister'])->name('register');
+// Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'viewLogin'])->name('login.index');
+// Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'attemptLogin'])->name('login');
+// Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
